6d87dee217f4002f2d17508f1bcee585b9f40302
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 21:01:03 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  
   include Dance
  
   attr_accessor :name
@@ -9,4 +10,5 @@ class Kid
   def initialize(name)
     @name = name
   end
+  
 end
