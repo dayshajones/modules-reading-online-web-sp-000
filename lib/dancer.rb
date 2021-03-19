886a5c62db9e78f134a67fc94760073e07a8e83a
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 21:04:43 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,17 +1,17 @@
+require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
+
+    include Dance
+
+    attr_accessor :name
   
-  include Dance
-  extend MetaDancing
-  extend FancyDance::ClassMethods
+    def initialize(name)
+      @name = name
+    end
+    
+    extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-  
 end
